249622eec4d571efe1af2d77d1c377af1f9c492a
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-03 18:00:03 +0700

    feat: penambahan halaman manajemen dokter untuk role admin

---
+++ lib/pages/admin/manajemen_dokter_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ManajemenDokterPage extends StatelessWidget {
+  const ManajemenDokterPage({super.key});
+
+  static const Color primaryColor = Color(0xFFB23A48);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin - Manajemen Dokter'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(FontAwesomeIcons.stethoscope, size: 60, color: primaryColor),
+            SizedBox(height: 16),
+            Text(
+              'Manajemen Dokter',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Kelola Data Dokter',
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
