2b8c6e650bf6de4b78258e0d42f86204da2afc8f
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-14 11:19:15 +0700

    feat: pendaftaran rute halaman admin dokter dan pengguna pada aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'pages/auth/login_page.dart';
 import 'pages/auth/register_page.dart';
 import 'pages/admin/admin_main_page.dart';
+import 'pages/admin/notifikasi_admin_page.dart';
+import 'pages/admin/master_spesialisasi_page.dart';
+import 'pages/admin/laporan_riwayat_page.dart';
 import 'pages/dokter/dokter_main_page.dart';
+import 'pages/dokter/patient_insight_page.dart';
+import 'pages/dokter/notifikasi_dokter_page.dart';
+import 'pages/dokter/pengaturan_dokter_page.dart';
 import 'pages/pengguna/pengguna_main_page.dart';
+import 'pages/pengguna/notifikasi_pengguna_page.dart';
+import 'pages/pengguna/konsultasi_dokter_page.dart';
+import 'pages/pengguna/edukasi_kulit_page.dart';
+import 'pages/pengguna/skin_check_question1_page.dart';
+import 'pages/pengguna/skin_check_question2_page.dart';
 
 void main() {
   runApp(const SkinoraApp());
@@ -42,8 +53,19 @@ class SkinoraApp extends StatelessWidget {
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
         '/admin': (context) => const AdminMainPage(),
+        '/admin/notifikasi': (context) => const NotifikasiAdminPage(),
+        '/admin/spesialisasi': (context) => const MasterSpesialisasiPage(),
+        '/admin/laporan': (context) => const LaporanRiwayatPage(),
         '/dokter': (context) => const DokterMainPage(),
+        '/dokter/patient-insight': (context) => const PatientInsightPage(),
+        '/dokter/notifikasi': (context) => const NotifikasiDokterPage(),
+        '/dokter/pengaturan': (context) => const PengaturanDokterPage(),
         '/pengguna': (context) => const PenggunaMainPage(),
+        '/pengguna/notifikasi': (context) => const NotifikasiPenggunaPage(),
+        '/pengguna/konsultasi': (context) => const KonsultasiDokterPenggunaPage(),
+        '/pengguna/edukasi': (context) => const EdukasiKulitPenggunaPage(),
+        '/pengguna/skin-check/pertanyaan-1': (context) => const SkinCheckQuestion1Page(),
+        '/pengguna/skin-check/pertanyaan-2': (context) => const SkinCheckQuestion2Page(age: '20'),
       },
     );
   }
