4b8504c6c8d909f675421226a3c91b41df46511e
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-24 00:00:30 +0700

    feat: tambahkan opsi konfigurasi Firebase

---
+++ lib/firebase_options.dart
@@ -0,0 +1,63 @@
+// File generated from android/app/google-services.json (project: skinora-app).
+// Run `flutterfire configure` again after registering iOS/Web apps to extend this file.
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+/// Default [FirebaseOptions] for use with your Firebase apps.
+///
+/// Example:
+/// ```dart
+/// import 'firebase_options.dart';
+/// // ...
+/// await Firebase.initializeApp(
+///   options: DefaultFirebaseOptions.currentPlatform,
+/// );
+/// ```
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'register the iOS app in the Firebase console (bundle id: '
+          'com.example.skinoraApp) then run the FlutterFire CLI again.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyCJ9n6wWrcZmqh-6a5EBCPbEtUZeDHNouI',
+    appId: '1:1018258328928:android:b1121084f71880cc5d6205',
+    messagingSenderId: '1018258328928',
+    projectId: 'skinora-app',
+    storageBucket: 'skinora-app.firebasestorage.app',
+  );
+}
