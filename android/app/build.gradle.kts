7550610d31598ad41802fb29e27fc2fea03b027c
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-24 05:43:01 +0700

    fix: perbaiki konfigurasi build Android (NDK, minSdk, Kotlin)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,15 +17,17 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.example.skinora_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
@@ -33,7 +35,7 @@ android {
         applicationId = "com.example.skinora_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
